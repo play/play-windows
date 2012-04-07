3390f41aaf39bd31dc3362217924d81e36b6ec0b
Paul Betts <paul@paulbetts.org>
2012-04-07 13:50:56 -0700

    Bootstrap the app

---
+++ Play/ViewModels/AppBootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akavache;
+using Ninject;
+using ReactiveUI;
+using ReactiveUI.Routing;
+
+namespace Play.ViewModels
+{
+    public class AppBootstrapper : ReactiveObject, IScreen
+    {
+        public IRoutingState Router { get; protected set; }
+
+        public AppBootstrapper(IKernel testKernel = null, IRoutingState router = null)
+        {
+            Kernel = testKernel ?? new StandardKernel();
+            Router = router ?? new RoutingState();
+
+            Kernel.Bind<AppBootstrapper>().ToConstant(this);
+            Kernel.Bind<IScreen>().ToConstant(this);
+
+            RxApp.ConfigureServiceLocator(
+                (type, contract) => Kernel.Get(type, contract),
+                (type, contract) => Kernel.GetAll(type, contract));
+
+            BlobCache.ApplicationName = "PlayForWindows";
+        }
+
+        public static IKernel Kernel { get; protected set; }
+    }
+}
