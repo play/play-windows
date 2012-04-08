d8b43bb86d8fe1950f616bb5d8a89ac44c8ba1d4
Paul Betts <paul@paulbetts.org>
2012-04-07 17:33:07 -0700

    Interface'ify bootstrapper, add method to get a RestClient

--- Play/ViewModels/AppBootstrapper.cs
+++ Play/ViewModels/AppBootstrapper.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Akavache;
 using Ninject;
 using ReactiveUI;
 using ReactiveUI.Routing;
+using RestSharp;
 
 namespace Play.ViewModels
 {
-    public class AppBootstrapper : ReactiveObject, IScreen
+    public interface IAppBootstrapper : IScreen
+    {
+        Task<IRestClient> GetAuthenticatedClient();
+    }
+
+    public class AppBootstrapper : ReactiveObject, IAppBootstrapper
     {
         public IRoutingState Router { get; protected set; }
 
@@ -19,14 +26,25 @@ namespace Play.ViewModels
             Kernel = testKernel ?? new StandardKernel();
             Router = router ?? new RoutingState();
 
-            Kernel.Bind<AppBootstrapper>().ToConstant(this);
             Kernel.Bind<IScreen>().ToConstant(this);
+            Kernel.Bind<IAppBootstrapper>().ToConstant(this);
 
             RxApp.ConfigureServiceLocator(
                 (type, contract) => Kernel.Get(type, contract),
                 (type, contract) => Kernel.GetAll(type, contract));
 
             BlobCache.ApplicationName = "PlayForWindows";
+        }
+
+        public async Task<IRestClient> GetAuthenticatedClient()
+        {
+            var blobCache = Kernel.Get<ISecureBlobCache>();
+            var baseUrl = await blobCache.GetObjectAsync<string>("BaseUrl");
+            var userName = await blobCache.GetObjectAsync<string>("Username");
+
+            var ret = new RestClient(baseUrl);
+            ret.AddDefaultParameter("login", userName);
+            return ret;
         }
 
         public static IKernel Kernel { get; protected set; }
