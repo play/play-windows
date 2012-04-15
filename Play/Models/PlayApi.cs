266284655fdd7d7546cbe1a98407a6f4c40731c5
Paul Betts <paul@paulbetts.org>
2012-04-14 18:21:18 -0700

    Stub out QueueSong API

--- Play/Models/PlayApi.cs
+++ Play/Models/PlayApi.cs
@@ -21,6 +21,7 @@ namespace Play.Models
         IObservable<BitmapImage> FetchImageForAlbum(Song song);
         IObservable<string> ListenUrl();
         IObservable<List<Song>> Queue();
+        IObservable<Unit> QueueSong(Song song);
         IObservable<Unit> Star(Song song);
         IObservable<Unit> Unstar(Song song);
         IObservable<List<Song>> Search(string query);
@@ -52,6 +53,11 @@ namespace Play.Models
             return client.RequestAsync<SongQueue>(rq).Select(x => {
                 return x.Data.songs.ToList();
             });
+        }
+
+        public IObservable<Unit> QueueSong(Song song)
+        {
+            throw new NotImplementedException();
         }
 
         public IObservable<Unit> Star(Song song)
