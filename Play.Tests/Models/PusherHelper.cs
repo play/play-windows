755e830e630a5070d5004e0e088b3fb2b2e4e163
Paul Betts <paul@paulbetts.org>
2012-04-14 23:21:37 -0700

    Wrap the ugly Pusher client API

---
+++ Play.Tests/Models/PusherHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Play.Models;
+using PusherClientDotNet;
+using ReactiveUI;
+using Xunit;
+
+namespace Play.Tests.Models
+{
+    public class PusherHelperTests
+    {
+        [Fact]
+        public void PusherIntegrationSmokeTest()
+        {
+            Func<Pusher> factory = () => new Pusher(IntegrationTestUrl.PusherApiKey);
+
+            var result = PusherHelper.Connect<Dictionary<string, object>>(factory, "now_playing_updates", "update_now_playing")
+                .Timeout(TimeSpan.FromMinutes(4.0), RxApp.TaskpoolScheduler)
+                .Take(1)
+                /*
+                .Select(x =>
+                    new Tuple<Song, List<Song>>(
+                        JsonConvert.DeserializeObject<Song>(x["now_playing"].ToString()),
+                        JsonConvert.DeserializeObject<SongQueue>(x["songs"].ToString()).songs))
+                */
+                .First();
+
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ Play/Models/PusherHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PusherClientDotNet;
+
+namespace Play.Models
+{
+    public static class PusherHelper
+    {
+        public static IObservable<T> Connect<T>(Func<Pusher> pusherFactory, string channel, string eventName)
+        {
+            var pusher = pusherFactory();
+            var disp = new CompositeDisposable();
+
+            return Observable.Create<T>(subj => {
+                bool hasCompleted = false;
+                try {
+                    pusher.Connect();
+                    disp.Add(Disposable.Create(pusher.Disconnect));
+
+                    var ch = pusher.Subscribe(channel);
+                    disp.Add(Disposable.Create(() => pusher.Unsubscribe(channel)));
+
+                    ch.Bind(eventName, x => {
+                        if (hasCompleted) return;
+                        subj.OnNext((T)x);
+                    });
+
+                    disp.Add(Disposable.Create(ch.Disconnect));
+                } catch (Exception ex) {
+                    subj.OnError(ex);
+                    hasCompleted = true;
+                }
+
+                return disp;
+            });
+        }
+    }
+}
