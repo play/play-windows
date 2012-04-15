0f2e0332a713b2b03e4afd7cf13844a14c2a0c66
Paul Betts <paul@paulbetts.org>
2012-04-14 17:36:52 -0700

    Make sure we fetch album art for all the images

--- Play.Tests/ViewModels/SearchViewModel.cs
+++ Play.Tests/ViewModels/SearchViewModel.cs
@@ -5,6 +5,7 @@ using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using Akavache;
 using FluentAssertions;
 using Moq;
@@ -25,15 +26,7 @@ namespace Play.Tests.ViewModels
         public void PlayApiShouldBeCalledOnPerformSearch()
         {
             var kernel = new MoqMockingKernel();
-            kernel.Bind<ISearchViewModel>().To<SearchViewModel>();
-            kernel.Bind<IBlobCache>().To<TestBlobCache>().Named("UserAccount");
-
-            kernel.GetMock<IPlayApi>()
-                .Setup(x => x.Search("Foo"))
-                .Returns(Observable.Return(new List<Song>() { new Song() { id = "12345" } }))
-                .Verifiable();
-
-            var fixture = kernel.Get<ISearchViewModel>();
+            var fixture = setupStandardFixture(kernel);
             fixture.PerformSearch.CanExecute(null).Should().BeFalse();
 
             fixture.SearchQuery = "Foo";
@@ -44,6 +37,52 @@ namespace Play.Tests.ViewModels
 
             fixture.SearchResults.Count.Should().Be(1);
             fixture.SearchResults[0].Model.id.Should().Be("12345");
+        }
+
+        [Fact]
+        public void DontThrashPlayApiOnMultipleSearchCalls()
+        {
+            var kernel = new MoqMockingKernel();
+            var fixture = setupStandardFixture(kernel);
+
+            fixture.SearchQuery = "Foo";
+            fixture.PerformSearch.Execute(null);
+            fixture.PerformSearch.Execute(null);
+            fixture.PerformSearch.Execute(null);
+
+            kernel.GetMock<IPlayApi>().Verify(x => x.Search("Foo"), Times.Once());
+        }
+
+        [Fact]
+        public void EnsureWeFetchAnAlbumCover()
+        {
+            var kernel = new MoqMockingKernel();
+            var fixture = setupStandardFixture(kernel);
+
+            fixture.SearchQuery = "Foo";
+            fixture.PerformSearch.Execute(null);
+
+            kernel.GetMock<IPlayApi>().Verify(x => x.Search("Foo"), Times.Once());
+            kernel.GetMock<IPlayApi>().Verify(x => x.FetchImageForAlbum(It.IsAny<Song>()), Times.Once());
+        }
+
+        static ISearchViewModel setupStandardFixture(MoqMockingKernel kernel)
+        {
+            kernel.Bind<ISearchViewModel>().To<SearchViewModel>();
+            kernel.Bind<IBlobCache>().To<TestBlobCache>().Named("UserAccount");
+            kernel.Bind<IBlobCache>().To<TestBlobCache>().Named("LocalMachine");
+
+            kernel.GetMock<IPlayApi>().Setup(x => x.Search("Foo"))
+                .Returns(Observable.Return(new List<Song>() {new Song() {id = "12345"}}))
+                .Verifiable();
+
+            var img = new BitmapImage();
+            kernel.GetMock<IPlayApi>().Setup(x => x.FetchImageForAlbum(It.IsAny<Song>()))
+                .Returns(Observable.Return(img))
+                .Verifiable();
+
+            var fixture = kernel.Get<ISearchViewModel>();
+            return fixture;
         }
     }
 }
