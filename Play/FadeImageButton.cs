26d32d3becaa112ac453f540d8a7a46df1984a99
Paul Betts <paul@paulbetts.org>
2012-04-18 21:51:39 -0700

    Add a fade-in/fade-out image button

---
+++ Play/FadeImageButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Play
+{
+    public class FadeImageButton : Button
+    {
+        public object HighlightContent {
+            get { return (object)GetValue(HighlightContentProperty); }
+            set { SetValue(HighlightContentProperty, value); }
+        }
+        public static readonly DependencyProperty HighlightContentProperty =
+            DependencyProperty.Register("HighlightContent", typeof(object), typeof(FadeImageButton), new UIPropertyMetadata(null));
+    }
+
+    public class FadeImageToggleButton : ToggleButton
+    {
+        public object HighlightContent {
+            get { return (object)GetValue(HighlightContentProperty); }
+            set { SetValue(HighlightContentProperty, value); }
+        }
+        public static readonly DependencyProperty HighlightContentProperty =
+            DependencyProperty.Register("HighlightContent", typeof(object), typeof(FadeImageButton), new UIPropertyMetadata(null));
+    }
+}
