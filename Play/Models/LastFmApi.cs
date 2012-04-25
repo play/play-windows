5ccef117c9eb25cad2dc65fa10f37c3923a1d904
Paul Betts <paul@paulbetts.org>
2012-04-24 22:08:59 -0700

    Stub out the last.fm API

---
+++ Play/Models/LastFmApi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace Play.Models
+{
+    public interface ILastFmApi : IReactiveNotifyPropertyChanged, IDisposable
+    {
+        bool CanScrobble { get; }
+        IObservable<Unit> BeginAuthorize(IObservable<Unit> retrySessionHint = null);
+        IObservable<Unit> Scrobble(Song song);
+    }
+
+    public class LastFmApiHelper : ReactiveObject, ILastFmApi
+    {
+        IDisposable inner;
+
+        public bool CanScrobble {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IObservable<Unit> BeginAuthorize(IObservable<Unit> retrySessionHint = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Unit> Scrobble(Song song)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            var disp = Interlocked.Exchange(ref inner, null);
+            if (disp != null) {
+                disp.Dispose();
+            }
+        }
+    }
+}
